ff62a49fd22a1739c4f41905eca26d459bc45d3c
hoanghai1608 <hoanghai160820099@gmail.com>
2024-12-10 10:45:06 +0700

    Add files via upload

---
+++ LoopWhile/src/Basic/dev/MainApp6.java
@@ -0,0 +1,12 @@
+package Basic.dev;
+
+public class MainApp6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i = 0;
+        int tong = 0;
+        while()
+	}
+
+}
